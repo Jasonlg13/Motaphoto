da28523e933787a79dce2f0e594b8d38c0a715d0
Jason <jason.leguen13@gmail.com>
2025-06-04 09:56:02 +0200

    Modale de contact

---
+++ template_parts/contact.php
@@ -0,0 +1,15 @@
+<div class="popup-overlay hidden">
+	<div class="popup-contact">
+		<div class="popup-title">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact.png"
+			alt="Contact <?php echo bloginfo('contact'); ?>">
+			<div class="popup-title"></div>
+			<div class="popup-title"></div>
+		</div>
+		<div class="popup-informations">	
+			<?php
+				echo do_shortcode('[contact-form-7 id="b43c68c" title="Formulaire de contact"]');
+			?>
+		</div>	
+	</div>
+</div>
